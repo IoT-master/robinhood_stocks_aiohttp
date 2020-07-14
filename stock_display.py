434ebdd67e8a7b710a11ed5a9f7ad8ebc904106a
Senh Mo Chuang <senhmo@gmail.com>
2020-07-14 01:18:43 -0500

    Fix Stock Profit Calculations

--- stock_display.py
+++ stock_display.py
@@ -11,19 +11,21 @@ class Usage(Robinhood):
             stocks = await self.get_stock_positions_from_account()
             self.clear_screen()
             sorted_stocks = sorted(
-                stocks.items(), key=lambda x: x[1]['status']['dB'], reverse=True)
-            total_value = 0
+                stocks.items(), key=lambda x: x[1]['status']['extended'] - float(x[1]['average_buy_price']), reverse=True)
+            total_profit = 0
             total_daily_profit = 0
+            total_value = 0
             for k, v in sorted_stocks:
                 stats = v['status']
                 average_buy_price = float(v['average_buy_price'])
                 profit_per_share = stats['extended'] - average_buy_price
                 db_profit_per_share = log(stats['extended'] / average_buy_price)
-                total_value += average_buy_price * float(v['quantity'])
-                total_daily_profit += float(v['quantity']) * profit_per_share
+                total_value += float(v['quantity']) * stats['extended']
+                total_profit += average_buy_price * float(v['quantity'])
+                total_daily_profit += float(v['quantity']) * (stats['extended'] - stats['adjusted_previous_close'])
                 print(
                     f"{k.rjust(8, ' ')}: [abp: {average_buy_price:8.2f}], [#: {float(v['quantity']):7.2f}] [pps: {profit_per_share:6.2f}], [dbpps: {db_profit_per_share:6.2f}] [last: {stats['last']:8.2f}], [bid: {stats['bid']:8.2f}], [ask: {stats['ask']:8.2f}], [real_last: {stats['extended']:8.2f}], [%: {stats['percent']:6.2f}], [dB: {stats['dB']:6.3f}]")
-            print("\033[36m" + f"Total Profits:{total_value:10.2f} Daily Profits: {total_daily_profit:.2f}" + "\033[0m")
+            print("\033[36m" + f"Total Value {total_value:10.2f} Total Profits:{total_profit:10.2f} Daily Profits: {total_daily_profit:.2f}" + "\033[0m")
             sleep(.5)
 
 
