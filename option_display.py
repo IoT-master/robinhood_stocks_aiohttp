e206ba50bb8bb06048fa330e8e7fb547d89b8f79
Senh Mo Chuang <senhmo@Senh-Mos-MacBook-Pro-13.local>
2020-07-15 23:16:47 -0500

    Fix daily profit calc

--- option_display.py
+++ option_display.py
@@ -1,6 +1,7 @@
 from time import sleep
-
+from dateutil.parser import parse
 from Robinhood import Robinhood
+from datetime import datetime
 
 
 class Usage(Robinhood):
@@ -22,7 +23,8 @@ class Usage(Robinhood):
                 if each[1]:
                     last_traded_price_dict[each[0]] = float(each[1]['last_extended_hours_trade_price']) if each[1][
                         'last_extended_hours_trade_price'] else float(each[1]['last_trade_price'])
-                    previous_closed_price_dict[each[0]] = float(each[1]['adjusted_previous_close'])
+                    previous_closed_price_dict[each[0]] = float(
+                        each[1]['adjusted_previous_close'])
 
             options_list = []
             for each_ticker in options_dict:
@@ -39,22 +41,29 @@ class Usage(Robinhood):
             # Sort Functionality
             sorted_options = sorted(
                 options_list, key=lambda x: x['price_change'], reverse=True)
-                # options_list, key=lambda x: x['quantity'] * (float(x['adjusted_mark_price']) * 100 - x['average_price']), reverse=True)
+            # options_list, key=lambda x: x['quantity'] * (float(x['adjusted_mark_price']) * 100 - x['average_price']), reverse=True)
 
             total_from_profits = 0
             total_daily_profit = 0
             total_value = 0
             for stats in sorted_options:
+                same_day = parse(
+                    stats['created_at']).date() == datetime.today().date()
                 last_traded_price = last_traded_price_dict[stats['ticker']] if stats[
-                                                                                   'ticker'] in last_traded_price_dict else 0
-                profit = stats['quantity'] * (float(stats['adjusted_mark_price']) * 100 - stats['average_price'])
+                    'ticker'] in last_traded_price_dict else 0
+                profit = stats['quantity'] * \
+                    (float(stats['adjusted_mark_price'])
+                     * 100 - stats['average_price'])
                 position = 'buy ' if stats['quantity'] > 0 else 'sell'
-                daily_profit_per_option = stats['quantity'] * (float(stats['adjusted_mark_price'])*100 - float(stats['previous_close_price'])*100)
+                daily_profit_per_option = stats['quantity'] * (float(
+                    stats['adjusted_mark_price'])*100 - float(stats['previous_close_price'])*100)
+                true_daily_profit = profit if same_day else daily_profit_per_option
                 total_from_profits += profit
-                total_daily_profit += daily_profit_per_option
-                total_value += stats['quantity'] * float(stats['adjusted_mark_price']) * 100
+                total_daily_profit += true_daily_profit
+                total_value += stats['quantity'] * \
+                    float(stats['adjusted_mark_price']) * 100
                 print(
-                    f"{stats['ticker'].rjust(5, ' ')} {stats['price_change']:5.2f} [Daily_Profit: {daily_profit_per_option:7.2f}] [TotProfit: {profit:8.2f}] [s at {last_traded_price:6.2f}] [v at {float(stats['adjusted_mark_price']):7.2f}] {stats['quantity']} {position} {stats['type']} at {stats['strike_price']:5.1f} {stats['expiration_date']} [delta: {stats['delta']}] [gamma: {stats['gamma']}] [iv: {stats['implied_volatility']}] [theta: {stats['theta']}] [rho: {stats['rho']}] [vega: {stats['vega']}]")
+                    f"{stats['ticker'].rjust(5, ' ')} {stats['price_change']:5.2f} [Daily_Profit: {true_daily_profit:7.2f}] [TotProfit: {profit:8.2f}] [s at {last_traded_price:6.2f}] [v at {float(stats['adjusted_mark_price']):7.2f}] {stats['quantity']} {position} {stats['type']} at {stats['strike_price']:5.1f} {stats['expiration_date']} [delta: {stats['delta']}] [gamma: {stats['gamma']}] [iv: {stats['implied_volatility']}] [theta: {stats['theta']}] [rho: {stats['rho']}] [vega: {stats['vega']}]")
             print(
                 "\033[36m" + f"[Total Value: {total_value:10.2f}] [Daily Profits: {total_daily_profit:.2f}] [Total Profits: {total_from_profits:10.2f}]" + "\033[0m")
             sleep(.5)
