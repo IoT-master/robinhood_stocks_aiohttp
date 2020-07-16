ea4fa56d1005b93bcce850f53d685eea96e01bd4
Senh Mo Chuang <senhmo@Senh-Mos-MacBook-Pro-13.local>
2020-07-16 01:07:24 -0500

    added aftermarket to watchlist

--- view_watchlist.py
+++ view_watchlist.py
@@ -1,6 +1,7 @@
 from Robinhood import Robinhood
 from time import sleep
 
+
 class Usage(Robinhood):
 
     async def main(self):
@@ -11,16 +12,20 @@ class Usage(Robinhood):
         while True:
             self.clear_screen()
             quotes_response = await self.get_quotes(watch_list)
-            quotes_response = filter(lambda x: x, quotes_response)
+            quotes_response = list(filter(lambda x: x, quotes_response))
             sorted_stocks = sorted(
-                quotes_response, key=lambda x: (float(x['last_trade_price']) - float(x['previous_close']))/float(x['previous_close']),
+                quotes_response, key=lambda x: (float(
+                    x['last_trade_price']) - float(x['previous_close']))/float(x['previous_close']),
                 reverse=True)
             for each_ticker in sorted_stocks:
                 last_known_price = float(each_ticker['last_trade_price'])
                 bid_price = float(each_ticker['bid_price'])
                 ask_price = float(each_ticker['ask_price'])
                 previous_close = float(each_ticker['previous_close'])
-                print(f"{each_ticker['symbol'].rjust(8, ' ')} [dP/dt: {last_known_price-previous_close:8.2f}] [last: {last_known_price:8.2f}], [bid: {bid_price:8.2f}], [ask: {ask_price:8.2f}]")
+                extended_price = float(each_ticker.get(
+                    'last_extended_hours_trade_price', 0)) - float(each_ticker['last_trade_price'])
+                print(
+                    f"{each_ticker['symbol'].rjust(8, ' ')} [dP/dt: {last_known_price-previous_close:8.2f}] [last: {last_known_price:8.2f}], [bid: {bid_price:8.2f}], [ask: {ask_price:8.2f}] {extended_price:8.2f}")
             sleep(.5)
 
 
